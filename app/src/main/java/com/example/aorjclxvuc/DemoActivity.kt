b6d095caf51943af2e8f451109fa58babcf69377
ericlam <eric.lam@keysocapp.com>
2022-03-16 17:39:22 +0800

    first init

---
+++ app/src/main/java/com/example/aorjclxvuc/DemoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aorjclxvuc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+    }
+}
